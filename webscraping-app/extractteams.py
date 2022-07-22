a0270e4935e983b7bbcf588cd7ef67bb421ecf97
arthur.azevedo <arthur.azevedo@accenture.com>
2022-07-22 15:31:23 -0300

    Links para a seção secundária - Fix Série D

--- webscraping-app/extractteams.py
+++ webscraping-app/extractteams.py
@@ -8,8 +8,19 @@ def get_teams_by_competition(competitioninfo: ItemInfo) -> List[ItemInfo]:
     soup = get_page_source_new(baseurl)
 
     items = []
-    for link in soup.select('#yw1 td.hauptlink a[title]'):
-        items.append(ItemInfo(link['href'], link['title'], link.text))
+    base_css_search = 'td.hauptlink a[href*=verein]'
+    # Pegando os links dos times na seção principal da página
+    links = soup.select(f'.large-8.columns {base_css_search}')
+    if (len(links) > 0):
+        for link in links:
+            items.append(ItemInfo(link['href'], link['title'], link.text))
+    else:
+        # Pegando os links dos times na seção secundária da página
+        for link in soup.select(base_css_search):
+            # Colocando o href no mesmo padrão dos links principais
+            href = link['href'].replace('spielplan', 'startseite')
+            items.append(ItemInfo(href, link['title'], link.text))
+
 
     print(f'{len(items)} times para o campeonato {competitioninfo.title}.\n')
     return items
